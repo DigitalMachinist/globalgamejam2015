0444f11f0b9da590acaab1b9cc71a1f502d7bfd3
Jeff Rose <jrose0@gmail.com>
2015-01-23 18:10:10 -0500

    Scaffolded the project directory.

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name globalgamejam2015App
+ * @description
+ * # globalgamejam2015App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('globalgamejam2015App', [
+    'ngAnimate',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name globalgamejam2015App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the globalgamejam2015App
+ */
+angular.module('globalgamejam2015App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
